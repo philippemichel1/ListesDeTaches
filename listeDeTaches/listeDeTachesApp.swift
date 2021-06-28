c855b146422c25784ba897bf68183bbdc1b52de0
Philippe MICHEL <philippe.michel1@gmail.com>
2021-06-28 22:12:33 +0200

    Initial Commit

---
+++ listeDeTaches/listeDeTachesApp.swift
@@ -0,0 +1,17 @@
+//
+//  listeDeTachesApp.swift
+//  listeDeTaches
+//
+//  Created by Philippe MICHEL on 28/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct listeDeTachesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
